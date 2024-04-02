25a182491cdc656113ce73d19aa1e5eb8969f89e
Aaron Hutchinson <111082932+L3vinax@users.noreply.github.com>
2024-04-02 10:37:09 -0700

    Update MultiTag.ps1

--- MultiTag.ps1
+++ MultiTag.ps1
@@ -1,5 +1,9 @@
+#This resources variable can be changed to anything that the get-azresource supports.  
 $resources = get-azresource -ResourceType Microsoft.HybridCompute/machines
+
+#Enter your tags as a Tag/Value pair.
 $tags = @{"Tag1"="Value1"; "Tag2"="Value2"}
+
 ForEach ($resource in $resources)
 {
     New-AzTag -ResourceId $resource.id -Tag $tags
